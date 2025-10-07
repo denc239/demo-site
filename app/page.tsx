13dcb15f9d77b2553a600ed295e0c200fbbc742a
denc239 <46786447+denc239@users.noreply.github.com>
2025-10-07 09:40:51 +0300

    Add homepage using Next.js + Tailwind

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gradient-to-r from-blue-500 via-purple-500 h-sveen-sm  text-center">
+      <h1 className="text-3 text-white">Hello, this is a stylish demo site</h1>
+    </main>
+  );
+}
